e703687c5eddeb4ac49cf9368568dd04124f2ef6
Swing <763263311@qq.com>
2023-04-05 21:18:58 +0800

    feat: 加载中文案显示处理

--- utils/src/main/java/com/zhangteng/utils/StateViewHelper.kt
+++ utils/src/main/java/com/zhangteng/utils/StateViewHelper.kt
@@ -180,8 +180,11 @@ open class StateViewHelper {
                 .apply { interpolator = LinearInterpolator() }
             mLoadImageView?.startAnimation(mAnimation)
 
-            if (mLoadingText != null) {
+            if (!mLoadingText.isNullOrEmpty()) {
+                mLoadTextView?.visibility = View.VISIBLE
                 mLoadTextView?.text = mLoadingText
+            } else {
+                mLoadTextView?.visibility = View.GONE
             }
             mProgressDialog?.setContentView(view)
             mProgressDialog?.setCancelable(true)
@@ -200,8 +203,11 @@ open class StateViewHelper {
                     mProgressDialog?.setOwnerActivity(activity)
             }
         } else {
-            if (mLoadingText != null && mLoadTextView != null) {
+            if (!mLoadingText.isNullOrEmpty()) {
+                mLoadTextView?.visibility = View.VISIBLE
                 mLoadTextView?.text = mLoadingText
+            } else {
+                mLoadTextView?.visibility = View.GONE
             }
             if (mLoadImageView != null) {
                 if (mAnimation == null) {
