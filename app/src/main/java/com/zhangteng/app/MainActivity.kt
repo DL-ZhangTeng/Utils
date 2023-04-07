62bb3a112ea680e1739aef606c08eefd88a7e032
Swing <763263311@qq.comm>
2023-04-07 14:48:51 +0800

    加载中弹窗演示

--- app/src/main/java/com/zhangteng/app/MainActivity.kt
+++ app/src/main/java/com/zhangteng/app/MainActivity.kt
@@ -2,11 +2,12 @@ package com.zhangteng.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.zhangteng.utils.R
+import com.zhangteng.utils.StateViewHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        StateViewHelper().showProgressDialog(this)
     }
 }
